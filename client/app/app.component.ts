f67e40386bce40ce5880d27d87bbe439a5b2384f
felipefschwade <felipefaeschwade@gmail.com>
2017-02-10 13:06:01 -0300

    Transferindo a logica e a exibição da lista de imagens do App Component para um novo componenet

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
 
 @Component({
     moduleId : module.id,
@@ -7,16 +6,6 @@ import { Http } from '@angular/http';
     templateUrl : "./app.component.html"
 })
 
-export class AppComponent 
-{
+export class AppComponent {
 
-    fotos: Object[] = [];
-
-    constructor(http: Http)
-    {
-        http.get("/v1/fotos")
-        .map(res => res.json())
-        .subscribe(fotos => this.fotos = fotos, 
-        erro => console.log(erro));
-    }
 }
