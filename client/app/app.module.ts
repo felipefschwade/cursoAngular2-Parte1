daa3be93a0fe7936975a58fff660e79d96cd9ab7
felipefschwade <felipefaeschwade@gmail.com>
2017-02-10 13:12:45 -0300

    Adicionando os novos componentes ao carregamento da página principal

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from "./app.component";
 import { FotoModule } from "./foto/foto.module";
 import { HttpModule } from "@angular/http";
 import { PainelModule } from "./painel/painel.module";
+import { ListagemComponent } from "./listagem/listagem.component"
+import { CadastroComponent } from "./cadastro/cadastro.component"
 import "rxjs/add/operator/map";
 
 
 @NgModule({
-    imports : [BrowserModule, FotoModule, HttpModule, PainelModule],
+    imports : [BrowserModule, FotoModule, HttpModule, PainelModule, ListagemComponent, CadastroComponent],
     declarations : [AppComponent],
     bootstrap : [AppComponent]
 })
