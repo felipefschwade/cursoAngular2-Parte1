e9c597df096bb2d221f8d99d9d7d48af03f7f120
felipefschwade <felipefaeschwade@gmail.com>
2017-02-14 13:45:11 -0300

    Importando o FotoComponent para mapeamento implicito

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { FotoComponent } from "../foto/foto.component"
 
 @Component({
     moduleId : module.id,
     selector : "cadastro",
     templateUrl : "./cadastro.component.html" 
 })
-export class CadastroComponent { }
+export class CadastroComponent { 
+
+    foto: FotoComponent = new FotoComponent();
+
+    cadastrar(event: Event)
+    {
+        event.preventDefault();
+    }
+
+}
