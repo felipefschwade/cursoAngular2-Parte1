7cf3d96fbdbf4d52dbd8401cbcacad4ba52d9d4a
felipefschwade <felipefaeschwade@gmail.com>
2017-02-14 01:42:11 -0300

    Adicionando o Pipe ao carregamento do modulo

--- client/app/foto/foto.module.ts
+++ client/app/foto/foto.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from "@angular/core";
 import { FotoComponent } from "./foto.component";
-
+import { FotoPipe } from "./foto.pipe"
 @NgModule({
-    declarations : [FotoComponent],
-    exports : [FotoComponent]
+    declarations : [FotoComponent, FotoPipe],
+    exports : [FotoComponent, FotoPipe]
 })
 export class FotoModule { }
