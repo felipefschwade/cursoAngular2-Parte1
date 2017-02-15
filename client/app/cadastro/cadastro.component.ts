d9841b01a4ca84e6af1d65a62d75f95f6bb23dc5
felipefschwade <felipefaeschwade@gmail.com>
2017-02-15 01:50:20 -0300

    Criando a lógica para cadastro de uma imagem via Ajax através do Método Post

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
-import { FotoComponent } from "../foto/foto.component"
+import { FotoComponent } from "../foto/foto.component";
+import { Http, Headers } from "@angular/http";
 
 @Component({
     moduleId : module.id,
@@ -9,11 +10,21 @@ import { FotoComponent } from "../foto/foto.component"
 export class CadastroComponent { 
 
     foto: FotoComponent = new FotoComponent();
+    htpp: Http;
+
+    constructor(http: Http)
+    {
+        this.htpp = http;
+    }
 
     cadastrar(event: Event)
     {
         event.preventDefault();
-        console.log(this.foto);
+        let headers = new Headers({'Content-Type' : 'application/json'});
+        this.htpp.post("v1/fotos", JSON.stringify(this.foto), {headers : headers})
+        .subscribe(() => console.log("Imagem Salva Com Sucesso"),
+        error => console.log(error));
+        this.foto = new FotoComponent();
     }
 
 }
